45e12faba37e06f283395edf9e2311811bbce37d
sudipX <sudip-basnet@outlook.com>
2025-09-13 17:58:39 +0545

    add 121

---
+++ 121.cpp
@@ -0,0 +1,13 @@
+/*
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minimum = prices[0],ans=0;
+        for(int i=1;i<prices.size();i++){
+            ans=max(ans,prices[i]-minimum);
+            minimum=min(prices[i],minimum);
+        }
+        return ans;
+    }
+};
+*/
