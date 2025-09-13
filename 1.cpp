e56c09323fd098cce73bc37ea2295b7a854b88b5
sudipX <sudip-basnet@outlook.com>
2025-09-13 11:22:05 +0545

    first commit

---
+++ 1.cpp
@@ -0,0 +1,19 @@
+/*
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        vector<pair<int,int>>a(n);
+        for(int i=0;i<n;i++){a[i].first=nums[i],a[i].second=i;}
+        sort(a.begin(),a.end());
+        int i=0,j=n-1;
+        while(i<j){
+            int sum=a[i].first+a[j].first;
+            if(sum==target)return {a[i].second,a[j].second};
+            else if(sum>target)j--;
+            else i++;   
+        }
+        return nums;
+    }
+};
+*/
