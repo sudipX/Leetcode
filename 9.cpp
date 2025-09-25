3ac8bf8da3745116cb708d34e7aa981c968e0ab7
sudipX <sudip-basnet@outlook.com>
2025-09-25 15:59:36 +0545

    add 9

---
+++ 9.cpp
@@ -0,0 +1,15 @@
+/*
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        long long s=x,m=0;
+        if(x<0)return false;
+        while (s){
+            m*=10;
+            m+=s%10;
+            s/=10;
+        }
+        return m==x;
+    }
+};
+*/
