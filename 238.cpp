50cc9ad441cf5bb82f5c95d7285f40bfc677fdd3
sudipX <sudip-basnet@outlook.com>
2025-09-25 22:33:32 +0545

    add 238

---
+++ 238.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums){
+        int n=nums.size();
+        vector<int>pref(n+1,1),suff(n+1,1);
+        for(int i=0;i<n;i++){
+            pref[i+1]=pref[i]*nums[i];
+            suff[n-i-1]=suff[n-i]*nums[n-i-1];
+        }
+        for(int i=0;i<n;i++){
+            nums[i]=pref[i]*suff[i+1];
+        }
+        return nums;
+    }
+};
