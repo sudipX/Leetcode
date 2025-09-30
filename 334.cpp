7e301999ebe10d3c684bf696a5330a04638fc909
sudipX <sudip-basnet@outlook.com>
2025-09-30 09:16:48 +0545

    add 334

---
+++ 334.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int first=INT_MAX,second=INT_MAX;
+        for(int n:nums){
+            if(n<=first)first=n;
+            else if(n<=second)second=n;
+            else return true;
+        }
+        return false;
+    }
+};
